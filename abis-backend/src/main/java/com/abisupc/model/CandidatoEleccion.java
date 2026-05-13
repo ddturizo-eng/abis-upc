43460de9223b221bdb6e59e7f0f9f2cf91744af0
ddturizo-eng <ddturizo@unicesar.edu.co>
2026-05-13 10:27:39 -0500

    feat(db): agregar modelos BiometriaVotante, CandidatoEleccion y EleccionRol

---
+++ abis-backend/src/main/java/com/abisupc/model/CandidatoEleccion.java
@@ -0,0 +1,84 @@
+package com.abisupc.model;
+
+/**
+ * Postulación de un candidato en una elección específica. Permite que un candidato se postule en múltiples elecciones.
+ */
+public class CandidatoEleccion {
+
+    private Long idCandidato;
+    private Long idEleccion;
+    private Integer numeroCampania;
+    private String cargo;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+
+    public Long getIdCandidato() {
+        return idCandidato;
+    }
+
+    public Long getId() {
+        return idCandidato;
+    }
+
+    public void setIdCandidato(Long idCandidato) {
+        this.idCandidato = idCandidato;
+    }
+
+    public Long getIdEleccion() {
+        return idEleccion;
+    }
+
+    public void setIdEleccion(Long idEleccion) {
+        this.idEleccion = idEleccion;
+    }
+
+    public Integer getNumeroCampania() {
+        return numeroCampania;
+    }
+
+    public void setNumeroCampania(Integer numeroCampania) {
+        this.numeroCampania = numeroCampania;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+}
